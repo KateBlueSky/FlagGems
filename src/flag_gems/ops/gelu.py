505069e56c5db1141b804f33b12ae2d7bbc99d1c
Mcgrievy, Kathleen <kathleen.mcgrievy@intel.com>
2024-10-10 13:54:05 -0700

    Update the pow function that's broken in xpu.

--- src/flag_gems/ops/gelu.py
+++ src/flag_gems/ops/gelu.py
@@ -27,7 +27,8 @@ def gelu_none(x):
 @triton.jit
 def gelu_tanh(x):
     output = (
-        0.5 * x * (1 + tanh(x * 0.79788456 * (1 + 0.044715 * pow(x.to(tl.float32), 2))))
+        0.5 * x * (1 + tanh(x * 0.79788456 * (1 + 0.044715 * tl.float32 * tl.float32)))
+        #0.5 * x * (1 + tanh(x * 0.79788456 * (1 + 0.044715 * pow(x.to(tl.float32), 2))))
     )
     return output
 
@@ -39,7 +40,8 @@ def gelu_backward_none(x, dy):
     scale2: tl.constexpr = 0.3989422803  # 1 / math.sqrt(2 * math.pi)
     x_fp32 = x.to(tl.float32)
     dydx = (
-        scale2 * x_fp32 * exp(-pow(scale1 * x_fp32, 2))
+        scale2 * x_fp32 * exp(-1 * (scale1 * x_fp32 * scale1 * x_fp32))
+        #scale2 * x_fp32 * exp(-pow(scale1 * x_fp32, 2))
         + 0.5 * erf(scale1 * x_fp32)
         + 0.5
     )
@@ -52,9 +54,11 @@ def gelu_backward_none(x, dy):
 def gelu_backward_tanh(x, dy):
     x_fp32 = x.to(tl.float32)
     # 0.79788456 = math.sqrt(2 / math.pi)
-    tanh_out = tanh(0.79788456 * x * (1 + 0.044715 * pow(x_fp32, 2)))
+    #tanh_out = tanh(0.79788456 * x * (1 + 0.044715 * pow(x_fp32, 2)))
+    tanh_out = tanh(0.79788456 * x * (1 + 0.044715 * x_fp32 * x_fp32))
     dydx = 0.5 * x * (
-        (1 - pow(tanh_out, 2)) * (0.79788456 + 0.1070322243 * pow(x_fp32, 2))
+        (1 - (tanh_out * tanh_out) * (0.79788456 + 0.1070322243 * x_fp32 * x_fp32)
+        #(1 - pow(tanh_out, 2)) * (0.79788456 + 0.1070322243 * x_fp32 * x_fp32)
     ) + 0.5 * (1 + tanh_out)
     dx = dydx * dy
     return dx
